febfa2f3f369f275b723c239a16107212eba5c2b
vishalsinghthakur8299-hub <vishalsinghthakur8299@gmail.com>
2025-11-13 15:20:39 +0530

    add file

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -3,13 +3,16 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     await mongoose.connect(
-      "mongodb+srv://rahuldangwal864:rahul008@cluster0.2treo2j.mongodb.net/college-erp"
+      "mongodb+srv://vishalsingh:vishal7545@cluster0.n0pbz9e.mongodb.net/?appName=Cluster0"
     );
     console.log("MongoDB connected successfully");
   } catch (error) {
     console.error("MongoDB connection failed:", error.message);
     process.exit(1);
+    console.log("connect db");
   }
 };
 
 module.exports = connectDB;
+
+
